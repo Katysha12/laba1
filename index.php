696f111e5931d4367507bce280776518bcb8277b
Катя <kate.kate.2004@bk.ru>
2023-09-24 23:48:49 +0300

    new commit

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<title>Document</title>
 </head>
 <body>
-	<h1 style="margin-top: 60; text-align: center;">Ошкина Екатерина</h1></br>
+	<h1 style="margin-top: 60; text-align: center;">Ошкина Екатерина 18 вариант</h1></br>
 	<h2 style="margin-top: 0; margin-bottom: 0px; text-align: center; ">Текст задачи:</h2></br>
 	<p style="margin-top: 0; text-align: center; font-size: 30px;">Вычислить объем треугольной пирамиды.</p></br>
 	<p style="margin-top: 10; text-align: center; font-size: 20px;"> Для решения задачи по формуле(V = ha² / 4√3) были взяты тестовые числа как а = 5 и h =  10
